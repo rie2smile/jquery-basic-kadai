ed94f8bc06387e9ff23ad1b71d9a2c80d4af5259
rie2smile <167359502+rie2smile@users.noreply.github.com>
2024-07-11 11:09:41 +0900

    jQ課題アップ

---
+++ kadai_jq_004/event.js
@@ -0,0 +1,10 @@
+$(window).on('load',function(){
+//「loadイベントが発生しました」とコンソールに出力
+console.log('loadイベントが発生しました')
+})
+
+$(window).on('scroll',function(){
+    //「scrollイベントが発生しました」とコンソールに出力
+    console.log('scrollイベントが発生しました')
+})
+
